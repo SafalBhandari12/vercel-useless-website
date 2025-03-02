79b795ae8714c8e006180fb7f3516d4755a3d1ef
Safal Bhandari <108601244+SafalBhandari12@users.noreply.github.com>
2025-03-02 22:10:46 +0530

    Fixed the code

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    // Check the environment to decide if we should allow or disallow indexing
-    const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
+const nextConfig = {};
 
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            // "index, follow" for production, otherwise "noindex, nofollow"
-            value: isProd ? "index, follow" : "noindex, nofollow",
-          },
-        ],
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+export default nextConfig;
