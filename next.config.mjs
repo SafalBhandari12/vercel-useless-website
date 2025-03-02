ffa85a2b1457eb6367f75d2458d423d8a9c6acd6
Safal Bhandari <108601244+SafalBhandari12@users.noreply.github.com>
2025-03-02 22:06:22 +0530

    Changed to index the project in the web

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    // Check the environment to decide if we should allow or disallow indexing
+    const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
 
-export default nextConfig;
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            // "index, follow" for production, otherwise "noindex, nofollow"
+            value: isProd ? "index, follow" : "noindex, nofollow",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
