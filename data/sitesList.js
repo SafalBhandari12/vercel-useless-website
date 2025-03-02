48be94d5e299687ced0b39521760333ce8b424cf
Safal Bhandari <108601244+SafalBhandari12@users.noreply.github.com>
2025-03-02 15:23:33 +0530

    Initial commit

---
+++ data/sitesList.js
@@ -0,0 +1,21 @@
+const sitesList = [
+  "https://sliding.toys/mystic-square/8-puzzle/daily/",
+  "https://longdogechallenge.com/",
+  "https://maze.toys/mazes/mini/daily/",
+  "https://optical.toys",
+  "https://paint.toys/calligram/",
+  "https://puginarug.com",
+  "https://memory.toys/classic/easy/",
+  "https://alwaysjudgeabookbyitscover.com",
+  "https://clicking.toys/flip-grid/neat-nine/3-holes/",
+  "https://weirdorconfusing.com/",
+  "https://checkbox.toys/scale/",
+  "https://binarypiano.com/",
+  "https://mondrianandme.com/",
+  "https://onesquareminesweeper.com/",
+  "https://cursoreffects.com",
+  "http://floatingqrcode.com/",
+  "https://thatsthefinger.com/",
+];
+
+export default sitesList;
